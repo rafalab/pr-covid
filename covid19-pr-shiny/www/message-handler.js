9ed369d63014b732a95baa6d4706ddf919e6901f
Rolando J. Acosta <roa310@g.harvard.edu>
2020-07-17 19:09:58 -0400

    Delete message-handler.js

--- covid19-pr-shiny/www/message-handler.js
+++
@@ -1,5 +0,0 @@
-Shiny.addCustomMessageHandler("testmessage",
-                              function(message) {
-                                alert(JSON.stringify(message));
-                              }
-);
