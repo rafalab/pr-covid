ca3dc3a37cff5efe35540d1173449f9ca5241e4e
Rafael A. Irizarry <rairizarry@gmail.com>
2021-01-23 22:04:27 -0500

    change to vaccine wrange

--- dashboard/wrangle.R
+++ dashboard/wrangle.R
@@ -58,8 +58,6 @@ sum7 <- function(d, y, k = 7)
 # -- Fixed values
 pr_pop <- 3193694 ## population of puerto rico
 
-first_vaccine <- make_date(2020, 12, 15)
-
 icu_beds <- 229 #if available beds is missing change to this
 
 first_day <- make_date(2020, 3, 12)
@@ -524,17 +522,6 @@ vaccines <- read_csv(url) %>%
   select(date, total_distributed, total_vaccinations, people_vaccinated, people_fully_vaccinated) %>%
   arrange(date) 
 
-##2020-12-13 was the day before the first vaccine. We know it was 0 then
-##next operation starts the dataset that day
-vaccines <- 
-  data.frame(date = first_vaccine - days(1)) %>% 
-  full_join(data.frame(date = first_vaccine - days(1) + weeks(2)), by = "date") %>%
-  full_join(vaccines, by = "date")
-vaccines$total_distributed[1] <- 0
-vaccines$total_vaccinations[1] <- 0
-vaccines$people_vaccinated[1] <- 0
-vaccines$people_fully_vaccinated[2] <- 0 ##2 because it's 14 days later
-
 ## fill in NAs
 for(j in which(names(vaccines)!="date")){
   for(i in 4:nrow(vaccines)){
@@ -550,7 +537,7 @@ hosp_mort <- left_join(hosp_mort, vaccines, by = "date")
 the_stamp <- now()
 save(first_day, alpha, the_stamp, 
      tests, tests_by_strata, cases,
-     hosp_mort, labs, pr_pop, first_vaccine,
+     hosp_mort, labs, pr_pop, 
      file = file.path(rda_path, "data.rda"))
 save(lab_tab, file = file.path(rda_path, "lab_tab.rda"))
 save(rezago, file = file.path(rda_path, "rezago.rda"))
