8474107cc3a2434021d19c1b81beedaac8da942c
Rafael A. Irizarry <rairizarry@gmail.com>
2021-01-28 00:16:14 -0500

    updating to work of server

--- coalicion/dashboard/build-informe.R
+++ coalicion/dashboard/build-informe.R
@@ -1,11 +1,11 @@
-Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc')
-fn <- paste0("informe-", lubridate::today(),".md")
+fn <- paste0("informe-", lubridate::today(),".html")
 dir <- "../../docs/"
-rmarkdown::render("informe.Rmd", output_format = "html_document", output_dir = "informes", 
+rmarkdown::render("informe.Rmd", output_format = "html_document", output_dir = dir, 
                   output_file = fn, clean = TRUE)
 
 
 fns <- list.files(dir, pattern = "informe")
+fns <- sort(fns, decreasing=TRUE)
 name <- gsub(".html", "", fns)
 cat("##  Informes Diarios Sobre COVID19 Coalición Científica de Puerto Rico\n\n", file = file.path(dir, "index.md"))
 for(i in seq_along(name)){
